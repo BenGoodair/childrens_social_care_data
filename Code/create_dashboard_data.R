77f9b5cbef7ff4bd9aa6079a88b15e6bbc2e893a
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2023-10-04 12:44:55 +0100

    what a y'all? hippies? you keep recycling shit

--- Code/create_dashboard_data.R
+++ Code/create_dashboard_data.R
@@ -5,6 +5,7 @@ characteristics <- create_characteristics_data()
 source("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Code/expenditure_cleaning_function.R")
 expenditure <- create_expenditure_data()
 
+dashboard_data <- rbind(outcomes, characteristics, expenditure)
 
-
+write.csv(dashboard_data, "C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/childrens_social_care_data/Final_Data/outputs/dashboard_data.csv")
 
