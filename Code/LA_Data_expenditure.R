f82fdcab705ba7a5f1c734b5292fd8a3fff6c2d4
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2023-10-04 10:52:06 +0100

    This life that we're destined to lead

--- Code/LA_Data_expenditure.R
+++ Code/LA_Data_expenditure.R
@@ -12,13 +12,20 @@ options(scipen=999)
 rm(list=setdiff(ls(), c("")))
 ####Expenditure data pre2014####
 
-outturn0809 <- read.csv("Data/outturn_0809.csv")
-outturn0910 <- read.csv("Data/outturn_0910.csv")
-outturn1011 <- read.csv("Data/outturn_1011.csv")
-outturn1112 <- read.csv("Data/outturn_1112.csv")
-outturn1213 <- read.csv("Data/outturn_1213.csv")
-outturn1314 <- read.csv("Data/outturn_1314.csv")
-ExpenditureData <- read.csv("Data/LA_Care_Expenditure_By_Ownership_OverTime.csv")
+outturn0809 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_0809.csv"),
+                       colClasses = "character")
+outturn0910 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_0910.csv"),
+                        colClasses = "character")
+outturn1011 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_1011.csv"),
+                        colClasses = "character")
+outturn1112 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_1112.csv"),
+                        colClasses = "character")
+outturn1213 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_1213.csv"),
+                        colClasses = "character")
+outturn1314 <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/outturn_1314.csv"),
+                        colClasses = "character")
+ExpenditureData <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Raw_Data/LA_level/LA_Spend/LA_Care_Expenditure_By_Ownership_OverTime.csv"),
+                        colClasses = "character")
 
 outturn0809 <- outturn0809 %>% dplyr::mutate(S52.Line.Reference. = str_replace(S52.Line.Reference., "\\s", "|")) %>% 
   tidyr::separate(S52.Line.Reference., into = c("LineNumber", "CYPServiceDescription"), sep = "\\|")%>%
@@ -63,20 +70,24 @@ outturn1011 <- outturn1011 %>%  dplyr::rename(LineNumber = Line,
   dplyr::mutate(ReportGroup = NA, year=2011)%>% dplyr::select(-ReturnStatus)
 
 outturn1112 <- outturn1112 %>%  dplyr::mutate(ReportGroup = NA, year=2012)
-outturn1213 <- outturn1213 %>%  dplyr::mutate(year=2013, CYPServiceDescription = sub(".*? ", "", outturn1213$CYPServiceDescription)
-)
-outturn1314 <- outturn1314 %>%  dplyr::mutate(year=2014, CYPServiceDescription = sub(".*? ", "", outturn1314$CYPServiceDescription)
-)
+
+outturn1213 <- outturn1213 %>%  dplyr::mutate(year=2013,
+                                              CYPServiceDescription = str_replace(CYPServiceDescription, "^[^ ]* ", ""))
+
+outturn1314 <- outturn1314 %>%  dplyr::mutate(year=2014, 
+                                              CYPServiceDescription = str_replace(CYPServiceDescription, "^[^ ]* ", ""))
+
 
 pre2014 <- rbind(outturn0809, outturn0910, outturn1011, outturn1112, outturn1213, outturn1314)
 
-oldlalookup <- read.csv("Data/oldlalookup.csv")
-pre2014 <- merge(oldlalookup, pre2014, by="LA", all=T)
+#oldlalookup <- read.csv("Data/oldlalookup.csv")
+#pre2014 <- merge(oldlalookup, pre2014, by="LA", all=T)
 
-pre2014 <- pre2014 %>%dplyr::rename(LAD19NM = LA.Name, Laold = LA,
-                                    Description = CYPServiceDescription )
+pre2014 <- pre2014 %>%dplyr::rename(LA_Name = LA.Name, LA.Number = LA,
+                                    Description = CYPServiceDescription)%>%
+  dplyr::mutate(LAD19CD=NA)
 
-ExpenditureData$year <- str_sub(ExpenditureData$?..time_period, start= -2)
+ExpenditureData$year <- str_sub(ExpenditureData$time_period, start= -2)
 ExpenditureData$year <-  paste("20", ExpenditureData$year, sep="")
 
 ExpenditureData <- ExpenditureData %>% dplyr::filter(geographic_level == "Local authority")%>%
@@ -94,7 +105,10 @@ ExpenditureData <- ExpenditureData %>% dplyr::filter(geographic_level == "Local 
                 NetCurrentExpenditure = as.numeric(NetCurrentExpenditure)*1000000  , 
                 GovGrantsInsideAEF  = as.numeric(GovGrantsInsideAEF)*1000000   ,  
                 GovGrantsOutsideAEF= as.numeric(GovGrantsOutsideAEF)*1000000,    
-                LEANetRevenueExpenditure= as.numeric(LEANetRevenueExpenditure)*1000000)
+                LEANetRevenueExpenditure= as.numeric(LEANetRevenueExpenditure)*1000000)%>%
+  dplyr::rename(LA_Name = LAD19NM,
+                LA.Number = Laold)%>%
+  dplyr::filter(year=="2015")
 totals <- ExpenditureData %>% dplyr::filter(ReportGroup=="Total")%>%dplyr::mutate(LineNumber=NA)
 nontotals  <- ExpenditureData %>% dplyr::filter(ReportGroup!="Total")%>%
   dplyr::mutate(Description = str_replace(Description, "\\s", "|")) %>% 
@@ -104,16 +118,28 @@ nontotals  <- ExpenditureData %>% dplyr::filter(ReportGroup!="Total")%>%
 
 fulldata <- rbind(totals, nontotals, pre2014)
 
-
 #Try and clean text to comparable (remove punct and capitalise)
-fulldata$LAD19NM <-  gsub('&','and',fulldata$LAD19NM)
-
-fulldata$LAD19NM <-  gsub('[[:punct:] ]+',' ',fulldata$LAD19NM)
 
 
-fulldata$LAD19NM <-  toupper(fulldata$LAD19NM)
+fulldata <- fulldata %>%
+  dplyr::mutate(LA_Name = gsub('&','and',LA_Name),
+                LA_Name = gsub('[[:punct:] ]+',' ',LA_Name),
+                LA_Name = toupper(LA_Name),
+                LA_Name = str_trim(LA_Name),
+                ReportGroup = toupper(ReportGroup))%>%
+  dplyr::rename(variable=Description,
+                category=ReportGroup,
+                LA_Code = LAD19CD)%>%
+  dplyr::filter(category=="CHILDREN LOOKED AFTER")%>%
+  dplyr::mutate(category = "Expenditure")%>%
+  dplyr::select(-Income, -NetCurrentExpenditure, -GovGrantsInsideAEF, -GovGrantsOutsideAEF,
+                -LEANetRevenueExpenditure, -LineNumber)%>%
+  tidyr::pivot_longer(cols = !c(LA_Name,LA.Number, LA_Code, year, variable, category),
+                      names_to = "subcategory", values_to = "number")
 
-fulldata$LAD19NM <-  str_trim(fulldata$LAD19NM)
+
+
+
 
 write.csv(fulldata, "Data/LA_Expenditure_Childrens_Services.csv")
 
