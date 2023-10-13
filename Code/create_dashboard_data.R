9fae8a18f8a54f2d3365c3a0c807d5e114fd1ecd
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2023-10-13 10:24:19 +0100

    all we wanna do is blow your mind just one more time

--- Code/create_dashboard_data.R
+++ Code/create_dashboard_data.R
@@ -7,5 +7,86 @@ expenditure <- create_expenditure_data()
 
 dashboard_data <- rbind(outcomes, characteristics, expenditure)
 
+source("https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Code/provider_cleaning_function.R")
+ProviderData <- create_provider_data()
+
+
+library(tidyverse)
+
+ProviderData <- ProviderData %>%
+  mutate(date = as.Date(Registration.date, format = "%d/%m/%Y"),
+         month = format(date, "%m/%y"),
+         time = as.integer(time_length(difftime(date, as.Date("2022-12-01")), "months")))
+
+Providernobs <- ProviderData %>%
+  dplyr::filter(Provision.type == "Children's home",
+                !is.na(Local.authority)) %>%
+  dplyr::select(time, Sector, URN, Local.authority) %>%
+  dplyr::distinct()
+
+nobsByIdih <- Providernobs %>%
+  dplyr::group_by(time, Local.authority, Sector) %>%
+  dplyr::summarize(nobs = n())
+
+nobsprive <- nobsByIdih %>%
+  dplyr::filter(Sector == "Private")
+
+nobsvol <- nobsByIdih %>%
+  dplyr::filter(Sector == "Voluntary")
+
+nobsla <- nobsByIdih %>%
+  dplyr::filter(Sector == "Local Authority")
+
+all_data <- tibble(Sector = "Private",
+                   Local.authority = rep(unique(nobsprive$Local.authority), each = 596),
+                   time = rep(-595:0, times = 145),
+                   er = 1)
+
+nobsprive <- nobsprive %>%
+  right_join(all_data, by = c("Sector", "time", "Local.authority")) %>%
+  dplyr::arrange(time)%>%
+  dplyr::group_by(Local.authority)%>%
+  mutate(nobs = ifelse(is.na(nobs), 0, nobs),
+         cumulative = cumsum(nobs))%>%
+  dplyr::ungroup()
+
+
+####yepppp!!!!#####
+
+
+
+all_data <- tibble(Sector = "Local Authority",
+                   Local.authority = rep(unique(nobsprive$Local.authority), each = 596),
+                   time = rep(-595:0, times = 145),
+                   er = 1)
+
+nobsla <- nobsla %>%
+  right_join(all_data, by = c("Sector", "time", "Local.authority")) %>%
+  dplyr::arrange(time)%>%
+  dplyr::group_by(Local.authority)%>%
+  mutate(nobs = ifelse(is.na(nobs), 0, nobs),
+         cumulative = cumsum(nobs))%>%
+  dplyr::ungroup()
+
+all_data <- tibble(Sector = "Voluntary",
+                   Local.authority = rep(unique(nobsprive$Local.authority), each = 596),
+                   time = rep(-595:0, times = 145),
+                   er = 1)
+
+nobsvol <- nobsvol %>%
+  right_join(all_data, by = c("Sector", "time", "Local.authority")) %>%
+  dplyr::arrange(time)%>%
+  dplyr::group_by(Local.authority)%>%
+  mutate(nobs = ifelse(is.na(nobs), 0, nobs),
+         cumulative = cumsum(nobs))%>%
+  dplyr::ungroup()
+
+nobs <- bind_rows(nobsla, nobsvol, nobsprive)
+
+nobs$Sector <- factor(nobs$Sector, levels = c("Private", "Local Authority", "Third Sector"))
+levels(nobs$Sector) <- c("For-profit", "Local Authority", "Third Sector")
+
+Providernobs$Sector <- factor(Providernobs$Sector, levels = c("Private", "Local Authority", "Third Sector"))
+
 write.csv(dashboard_data, "C:/Users/benjamin.goodair/OneDrive - Nexus365/Documents/GitHub/childrens_social_care_data/Final_Data/outputs/dashboard_data.csv")
 
